627bffd22f6879654134db21c61390f0a7bf9c27
marciogabrielsf <marciogdsf@gmail.com>
2023-05-20 12:36:20 -0300

    Refatorando o código utilizando mutex. Forma mais eficiente de verificar cada mensagem. um processo agora só verifica depois do outro (em sequencia)

--- Trabalho_SO_Parte1.py
+++ Trabalho_SO_Parte1.py
@@ -1,54 +1,48 @@
-from multiprocessing import Process, Array, Queue
+from multiprocessing import Lock, Process, Array, Queue
 from os import system
 
-def process1(shm, queue_processo_1: Queue):
-    # pega o último valor da shared memory. serve para comparar se ela foi alterada.
-    lastShmValue = shm.value.decode()
-    # inicia um loop entre os processos
+def process1(shm, queue_processo_1: Queue, lock):
+    
+    # inicia um loop
     while True:
-        
-        
-        #receive
-        
-        # fica comparando o valor da shm atual com o último valor alterado da shared memory
-        shmActual = shm.value.decode()
-        if(lastShmValue != shmActual): #caso detecte que foi alterado, significa que a shm recebeu uma mensagem do outro processo.
-            print(" Processo 1 - Recebeu:", shmActual)
-            lastShmValue = shmActual
+        with lock: # a trava serve para que os processos funcionem necessariamente em sequencia, um lê/escreve, dai em seguida o outro lê/escreve
             
-        # send   
-        
-        # checa se a queue não está vazia
-        if(not queue_processo_1.empty()):
-            terminalMessage1 = queue_processo_1.get() #pega o que tem na fila
-            if(terminalMessage1): #if pra evitar erros.
-                shm.value = terminalMessage1.encode(); #pega o que estava na fila (que foi recebido do terminal), encoda em bytecode e coloca na shm.
-                print(' Processo 1 - Enviou:', terminalMessage1) #print para avisar que o processo colocou uma mensagem na shm
-                lastShmValue = terminalMessage1 #reseta o último valor da shm.
+            #receive
+            
+            if(shm.value != b''): #caso detecte que shm não é nula, significa que a shm recebeu uma mensagem do outro processo.
+               print(" Processo 1 - Recebeu:", shm.value.decode()) #decoda e printa a mensagem
+               shm.value = b''
+
+           # send
+           
+           # checa se a queue não está vazia
+            if(not queue_processo_1.empty()):
+               terminalMessage1 = queue_processo_1.get() #pega o que tem na fila
+               if(terminalMessage1): #if pra evitar erros.
+                   shm.value = terminalMessage1.encode(); #pega o que estava na fila (que foi recebido do terminal), encoda em bytecode e coloca na shm.
+                   print(' Processo 1 - Enviou:', terminalMessage1) #print para avisar que o processo colocou uma mensagem na shm
     
 
-def process2(shm, queue_processo_2: Queue):
-    # pega o último valor da shared memory. serve para comparar se ela foi alterada.
-    lastShmValue = shm.value.decode()
-    # fica comparando o valor da shm atual com o último valor alterado da shared memory
-    while True:
-        #receive
-        shmActual = shm.value.decode()
-        #caso detecte que foi alterado, significa que a shm recebeu uma mensagem do outro processo.
-        if(lastShmValue != shmActual):
-            print(" Processo 2 - Recebeu:", shmActual)
-            lastShmValue = shmActual
-            
-        # send   
+def process2(shm, queue_processo_2: Queue, lock):
 
-        
-        # checa se a queue não está vazia
-        if(not queue_processo_2.empty()):
-            terminalMessage2 = queue_processo_2.get()
-            if(terminalMessage2): #if pra evitar erros.
-                shm.value = terminalMessage2.encode(); #pega o que estava na fila (que foi recebido do terminal), encoda em bytecode e coloca na shm.
-                print(' Processo 2 - Enviou:', terminalMessage2) #print para avisar que o processo colocou uma mensagem na shm
-                lastShmValue = terminalMessage2 #reseta o último valor da shm.
+        # inicia um loop
+        while True:
+         with lock: # a trava serve para que os processos funcionem necessariamente em sequencia, um lê/escreve, dai em seguida o outro lê/escreve
+
+            #receive
+            
+            if(shm.value != b''): #caso detecte que shm não é nula, significa que a shm recebeu uma mensagem do outro processo.
+                print(" Processo 2 - Recebeu:", shm.value.decode()) #decoda e printa a mensagem
+                shm.value = b'' #põe a shm novamente em nula.
+
+            # send   
+
+            # checa se a queue não está vazia
+            if(not queue_processo_2.empty()):
+                terminalMessage2 = queue_processo_2.get()
+                if(terminalMessage2): #if pra evitar erros.
+                    shm.value = terminalMessage2.encode(); #pega o que estava na fila (que foi recebido do terminal), encoda em bytecode e coloca na shm.
+                    print(' Processo 2 - Enviou:', terminalMessage2) #print para avisar que o processo colocou uma mensagem na shm
     
 
 if __name__ == '__main__':
@@ -59,11 +53,11 @@ if __name__ == '__main__':
     queue_processo_1 = Queue()
     queue_processo_2 = Queue()
 
-
+    lock = Lock() #cria o mutex
 
     # criar os dois processos com os devidos parâmetros (shared memory, fila de input do usuário)
-    p1 = Process(target=process1, args=(shm, queue_processo_1))
-    p2 = Process(target=process2, args=(shm, queue_processo_2))
+    p1 = Process(target=process1, args=(shm, queue_processo_1, lock))
+    p2 = Process(target=process2, args=(shm, queue_processo_2, lock))
 
     # iniciar os dois processos
     p1.start()
