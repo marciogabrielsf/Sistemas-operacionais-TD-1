0f4dabe0aa46906facea97bbddd267def4cd113a
marciogabrielsf <marciogdsf@gmail.com>
2023-05-26 22:29:02 -0300

    SO_Completo

--- Trabalho_SO_parte2.py
+++ Trabalho_SO_parte2.py
@@ -1,110 +1,185 @@
-from multiprocessing import Process, Array, Queue, Lock
+'''bibliotecas usadas para manipulação dos processos, shm e mutex'''
+from multiprocessing import shared_memory, Lock
 from os import system
+import multiprocessing
 
 
-def readFile(filename): #função que lê o arquivo (apenas para refatoração)
-    with open(filename, mode='rb') as file_obj: #lê o arquivo em modo "rb" (binário)
-        name = file_obj.name
-        content = file_obj.read()
-        return content, name
+def readFile(filename):
+    '''função que pega o conteudo do arquivo e retorna em bytes'''
+    with open(filename, mode='rb') as file_obj: 
+        conteudo = file_obj.read() 
+        return conteudo
 
 
 
+def processo1_send(filename, lock):
 
-def process1(shm , queue_processo_1: Queue, lock):
-    while True:
-        #receive
+        '''Função processo 1 envia:
+           Recebe o arquivo e converte em bytes;
+           acessa a memoria compartilhada pelo nome; 
+           copia o conteudo do arquivo para a memória compartilhada;
+           fecha a memoria compartilhada;
+           bloqueia p/ garantir a exclusão mutua.'''
+
+        lock.acquire()
+        conteudo = readFile(filename)
         
-        with lock: #trava o mutex
-            if(shm.value != b''): #caso a shm seja diferente de vazia
-                    print(" Processo 1 - Recebeu:", shm.value.decode()) #printa o conteudo do arquivo decodado
-                    shm.value = b'' #torna a shm vazia novamente.
 
-            # send   
+        shmm = shared_memory.SharedMemory('s')
+        
+        conteudo_bytes = (len(conteudo)+4).to_bytes(4, 'little') 
+        
+        buffer = conteudo_bytes + conteudo
+        shmm.buf[:len(conteudo) + len(conteudo_bytes)] = buffer
+        
+        print("arquivo enviado para Ana!")
+        shmm.close()
+        
+       
 
-            if(not queue_processo_1.empty()):
-                  terminalMessage1 = queue_processo_1.get() 
-                  if(terminalMessage1):
-                      try:
-                          content = readFile(terminalMessage1) #le o arquivo usando uma função la em cima, que retorna o conteudo do arquivo em binário
-                          shm.value = content[0]; #seta na shm o retorno do "content" que a função readFile retorna.
-                          print(' Processo 1 - Enviou:', content[1]) #printa o "name" que a função readFile retorna.
-                      except FileNotFoundError: #tratamento de erros
-                          print('Diretório invalido.')
-                      except Exception as err:
-                          print(err)
+
+def processo1_receive(lock):
     
+        '''Função em que o processo 1 recebe: 
+            Acessa a memoria compartilhada pelo nome;
+            percorre a memoria e copia o conteudo até chegar em '\0' e parar a leitura;
+            pega o conteudo do buf da memoria e copia para variável conteudo;
+            escreve o conteúdo copiado da memoria para dentro do arquivo. '''
+     
 
-def process2(shm , queue_processo_2: Queue, lock):
-    while True:
-        #receive
-        with lock: #trava o mutex
-            if(shm.value != b''): #caso a shm seja diferente de vazia
-                    print(" Processo 2 - Recebeu:", shm.value.decode()) #printa o conteudo do arquivo decodado
-                    shm.value = b'' #torna a shm vazia novamente.        
-            # send   
+        
+        shmm = shared_memory.SharedMemory('s')
 
-            if(not queue_processo_2.empty()):
-                     terminalMessage1 = queue_processo_2.get() 
-                     if(terminalMessage1):
+        conteudo_length = int.from_bytes(bytes(shmm.buf[:4]), 'little')
+             
+        conteudo = bytes(shmm.buf[4:conteudo_length])
 
-                         try:
-                             content = readFile(terminalMessage1) #le o arquivo usando uma função la em cima, que retorna o conteudo do arquivo em binário
-                             shm.value = content[0]; #seta na shm o retorno do "content" que a função readFile retorna.
-                             print(' Processo 2 - Enviou:', content[1]) #printa o "name" que a função readFile retorna.
-                         except FileNotFoundError: #tratamento de erros
-                             print('Diretório invalido.')
-                         except Exception as err:
-                             print(err)
+        arquivo = open('recebido.jpg', 'wb')
+        arquivo.write(conteudo)
+        print('Pedro Recebeu: ', conteudo)
 
-if __name__ == '__main__':
-    # create shared memory
-    shm = Array('c',2048,  lock=True)
-    
-    
-    queue_processo_1 = Queue() #cria a fila para integrar o terminal com o processo 1.
-    queue_processo_2 = Queue() #cria a fila para integrar o terminal com o processo 2.
+        lock.release()
+        arquivo.close()
+        shmm.close()
+       
 
-    lock = Lock() #cria o mutex
 
-    p1 = Process(target=process1, args=(shm, queue_processo_1, lock)) #cria o processo 1
-    p2 = Process(target=process2, args=(shm, queue_processo_2, lock)) #cria o processo 2
 
-    p1.start() #inicia o processo 1
-    p2.start()# inicia o processo 2 
-    
-    system('cls') #limpa a tela
-    while(True):
+def processo2_send(filename, lock):
 
-        print('Qual processo voce deseja Selecionar? \n 1- Processo 1 \n 2- Processo 2 \n 0- Fechar')
-        option = input()
-        match option: #switch case para o menu
+        '''função em que o processo 2 envia:
+           Função que recebe o arquivo e converte em bytes;
+           acessa a memoria compartilhada pelo nome; 
+           copia o conteudo do arquivo para a memória compartilhada;
+           fecha a memoria compartilhada;
+           bloqueia p/ garantir a exclusão mutua.'''
+        
+        
+        lock.acquire()
+        conteudo = readFile(filename)
+
+        shmm = shared_memory.SharedMemory('s')
+
+        conteudo_bytes = (len(conteudo)+4).to_bytes(4, 'little') 
+        
+        buffer = conteudo_bytes + conteudo
+        shmm.buf[:len(conteudo) + len(conteudo_bytes)] = buffer
+        print("arquivo enviado para Pedro!")
+        
+        shmm.close()
+        
+        
+
+
+
+
+def processo2_receive(lock):
+
+        '''Função onde o processo 2 recebe: 
+            Acessa a memoria compartilhada pelo nome;
+            percorre a memoria e copia o conteudo até chegar em '\0' e parar a leitura;
+            pega o conteudo do buf da memoria e copia para variável conteudo;
+            escreve o conteúdo copiado da memoria para dentro do arquivo. '''
+        
+        lock.release()
+        shmm = shared_memory.SharedMemory('s')
+        
+        conteudo_length = int.from_bytes(bytes(shmm.buf[:4]), 'little')
+             
+        conteudo = bytes(shmm.buf[4:conteudo_length])
+      
+        arquivo = open('recebido.jpg', 'wb')
+        arquivo.write(conteudo)
+        print('Ana recebeu: ', conteudo)
+
+        
+        
+        # arquivo.close()
+        shmm.close()
+
+        
+       
+
+def taskTwoMenu():
+
+        '''Cria a memoria compartilhada;
+        '''
+        shm = shared_memory.SharedMemory(name='s', create=True, size=2048)
+        lock = Lock()
+        ''' Executa o loop para decidir quem vai enviar a mensagem '''
+        
+        while(True):
+
+          print("Quem vai realizar o envio: \n 1- Pedro \n 2- Ana \n 0 - Sair")
+          in_process = input()
+
+         
+          match in_process: 
             case '1':
-                system('cls')
-                print('Digite o Diretório do arquivo para mandar para o processo 2:')
-                message1 = input() #pega o input do usuário
-                system('cls')
-                queue_processo_1.put(message1) #coloca o que o user digitou na fila para ser lido lá no processo
-                
+              system('cls')
+              print("Digite o nome do arquivo e sua extensão: ")
+              arq = input()
+              system('cls')
+          
+              p1 = multiprocessing.Process(target=processo1_send, args=(arq, lock,))
+              p2 = multiprocessing.Process(target=processo2_receive, args=(lock,))
+
+              p1.start()
+              p2.start()
+              
+              '''sicroniza a execução dos processos'''
+              p1.join()
+              p2.join()
+
+              # system('cls')
+
+              break
+
             case '2':
-                system('cls')
-                print('Digite o Diretório do arquivo para mandar para o processo 1')
-                message2 = input()
-                system('cls')
-                queue_processo_2.put(message2) #coloca o que o user digitou na fila para ser lido lá no processo
+              system('cls')
+              print("Digite o nome do arquivo e sua extensão: ")
+              arq_2 = input()
+              p2 = multiprocessing.Process(target=processo2_send, args=(arq_2, lock))
+              p1 = multiprocessing.Process(target=processo1_receive, args=(lock,))
+              system('cls')
+              p2.start()
+              p1.start()
+
+              '''sicroniza a execução dos processos'''
+              p2.join()
+              p1.join()
+
+              # system('cls')
+
+              break
             case '0':
-                break
-                
+              break
+           
             case _:
-                system('cls')
-                print('- digita algo que preste seu inutil')
-                
-                
+              system('cls')
+              print("Selecione um valor válido! ")
 
 
-    # finalizar os dois processos
-    p1.terminate()
-    p2.terminate()
+          shm.close()
 
-    # limpar a shared memory
-    shm.value = b''
+       
